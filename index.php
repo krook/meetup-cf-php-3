b7f26db9aba67f1916ebcbc8a5799f6e2bf63771
Daniel Krook <krook@us.ibm.com>
2014-09-23 16:29:55 -0400

    First commit of NYCCF / NYPHP meetup samples.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Cloud Foundry zero downtime deploy</title>
+  <meta name="description" content="Cloud Foundry demo">
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  
+  <div id="img">
+      <img src="http://cloudfoundry.org/images/logo_org.png"/>
+  </div>
+
+  <h1>
+    Version 2
+  </h1>
+  
+</body>
+</html>
